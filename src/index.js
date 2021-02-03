284ae9bc7a0f206fa32297389e046cd1f40e3677
Kirshach <kirshach@mail.ru>
2021-02-03 23:19:18 +0300

    homepage component added

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./components/App/App";
+
+const movie = {
+  title: `The Grand Budapest Hotel`,
+  genre: `Drama`,
+  releaseYear: 2014,
+};
+
+ReactDOM.render(<App {...movie} />, document.getElementById(`root`));
